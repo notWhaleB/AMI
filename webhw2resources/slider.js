c336aa9c59245007ebdceee698e78a71bf7a0054
Nikita <o0oflashero0o@gmail.com>
2015-03-03 00:23:16 +0300

    Firefox fix.

--- webhw2resources/slider.js
+++ webhw2resources/slider.js
@@ -4,16 +4,20 @@ function slide(value) {
     if (!isActive) {
         isActive = true;
         document.getElementById('main-slider').style.left = "-" + 100 * value + "vw";
-        setTimeout(function(){isActive = false}, 1000);
+        setTimeout(function(){isActive = false}, 1100);
 
         if (value == 4) {
             setTimeout(function() {
+                document.getElementById('main-slider').style.transitionDuration = "0s";
                 document.getElementById('main-slider').style.webkitTransitionDuration = "0s";
-                document.getElementById('main-slider').style.left = "0";
                 setTimeout(function() {
+                    document.getElementById('main-slider').style.left = "0";
+                }, 20);
+                setTimeout(function() {
+                    document.getElementById('main-slider').style.transitionDuration = "1s";
                     document.getElementById('main-slider').style.webkitTransitionDuration = "1s";
-                }, 1);
-            }, 1000);
+                }, 40);
+            }, 1020);
         }
     }
 }
